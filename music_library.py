9505661ecf67fda72a9edd3ea33ece1cec80b1bd
SauryaPratikMukherji <sauryapratik.mukherji35icse2022@gmail.com>
2026-04-15 11:07:57 +0530

    Add music library with YouTube links

---
+++ music_library.py
@@ -0,0 +1,6 @@
+music={
+    "stealth":"https://www.youtube.com/results?search_query=stealth+by+akash+gandhi",
+    "cartoon":"https://www.youtube.com/watch?v=gUQDq9ezuJQ",
+    "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "march":"https://www.youtube.com/watch?v=EDgaPJYe4Tg"
+}
